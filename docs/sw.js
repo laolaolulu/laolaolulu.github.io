a2280052841d1ec47af6a8502636b09fadfc01e9
laolaolulu <33310261+laolaolulu@users.noreply.github.com>
2023-02-09 03:28:19 +0000

    初始化

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230209.100015';
+const cacheName = 'chirpy-20230209.112711';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
