206d8d90c74a5d51be45818cb2cfc8d66ea3a266
laolaolulu <33310261+laolaolulu@users.noreply.github.com>
2023-02-09 07:17:00 +0000

    test

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230209.141616';
+const cacheName = 'chirpy-20230209.151651';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
