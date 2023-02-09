cc67e32b870584a4124d1a64fdd12da3cf095cdc
laolaolulu <33310261+laolaolulu@users.noreply.github.com>
2023-02-09 06:16:59 +0000

    测试写第一个博客

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230209.112711';
+const cacheName = 'chirpy-20230209.141616';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
