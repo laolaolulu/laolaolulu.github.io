cfb8c1c8bd5eab1d6b88e748c0581d45979eca40
laolaolulu <33310261+laolaolulu@users.noreply.github.com>
2023-02-10 03:14:10 +0000

    添加评论功能

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230210.104354';
+const cacheName = 'chirpy-20230210.111355';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
